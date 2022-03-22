2d35c7bb48d22c5f36f2505b492f151d41f7beb9
Maks <kmv80@yandex.ru>
2022-03-22 19:34:50 +0100

    add translate

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -33,12 +33,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'herokuapp.com',
     '127.0.0.1',
     'localhost',
+    'webserver',
 ]
 
 
